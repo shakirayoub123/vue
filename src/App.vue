bc3dec531f0143bfd4e8a490bdd69e2a94825e83
shakirayoub123 <shakir.ayoub@ladybirdweb.com>
2022-12-26 13:27:13 +0530

    vuex

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <base-container title="Vuex">
+  <h2>{{ counter}}</h2>
+  <button @click="clickMe">Add 10</button>
+  <change-counter></change-counter>  
+</base-container>
+ 
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+import changeCounter from './components/changeCounter.vue';
+
+export default {
+  components: {
+    BaseContainer,
+    changeCounter,
+  },
+  computed:{
+   counter(){
+    return this.$store.state.counter
+   }
+  },
+  methods:{
+    clickMe(){
+      // this.$store.state.counter++
+      this.$store.commit('increament')
+      // this.$store.commit('increase', {value:10})
+    // alternative method we can also write this in this way also
+    // this.$store.commit({
+    //   type:'increase',
+    //   value:10
+    // });
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/store.js
@@ -0,0 +1,18 @@
+import {createStore} from 'vuex';
+
+const store=createStore({
+   state(){
+    return{
+  counter:0
+    }
+   },
+   mutations:{
+    increament(state){
+      state.counter=state.counter+2
+    },
+    increase(state,payload){
+      state.counter=state.counter+payload.value
+    }
+   }
+})
+export default store
